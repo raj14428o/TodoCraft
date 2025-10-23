0684db43f2095aad9cd3a67d971b8143aa569e7f
Raj Kesharwani <rajkesharwani14428o@gmail.com>
2025-10-23 09:28:03 +0530

    todoItem width Adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   return (
     <ToDoProvider value={{todos, addToDo , updateToDo , deleteToDo , toggleComplete}}>
     <div>
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                <div className="w-full max-w-5xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                        <TodoForm/>
